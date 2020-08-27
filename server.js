da7e4d5a02802d2a01b9ed973fef49edd249df12
Henry Chung <chungthehao@gmail.com>
2020-08-27 16:11:07 +0700

    Creating Routes & Responses In Express

--- server.js
+++ server.js
@@ -7,6 +7,32 @@ dotenv.config({ path: './config/config.env' });
 // Initializa app variable
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show the bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create a new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update the bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete the bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 const env = process.env.NODE_ENV;
 
